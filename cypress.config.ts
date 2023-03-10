4da03321cfac04ac6176f28d324f69caa5300236
Olena Tsvietkova <otsvietkova@jappware.com>
2023-03-10 13:11:17 +0200

    Add playwright

--- cypress.config.ts
+++ cypress.config.ts
@@ -2,6 +2,8 @@ import { defineConfig } from "cypress";
 
 export default defineConfig({
   e2e: {
+    baseUrl: 'http://localhost:4200',
+    specPattern: 'cypress/e2e/tests/**/*.cy.{js,jsx,ts,tsx}',
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
